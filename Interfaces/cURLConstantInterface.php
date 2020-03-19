602e60508df63817ff270e030fc1b1857513d4e2
_ <firminchanteux@gmail.com>
2020-03-19 13:55:07 +0100

    add Application Json

--- Interfaces/cURLConstantInterface.php
+++ Interfaces/cURLConstantInterface.php
@@ -14,9 +14,10 @@ interface cURLConstantInterface {
     const APPLICATION = 'Content-Type: application/x-www-form-urlencoded';
     const MULTIPART = 'Content-Type: multipart/form-data';
     const TEXT = 'Content-Type: text/plain';
+    const APPLICATION_JSON = 'Content-Type: application/json';
 
     const METHODS = [self::GET, self::POST];
     const ENCODINGS = [self::JSON, self::_STRING, self::_ARRAY];   
     const RESPONSES = [self::JSON];
-    const CONTENT_TYPES = [self::APPLICATION, self::MULTIPART, self::TEXT];
+    const CONTENT_TYPES = [self::APPLICATION, self::MULTIPART, self::TEXT, self::APPLICATION_JSON];
 }
